d5c1ff32893d52d898f82904115d41affacf9744
Audrey King <audreymking1@gmail.com>
2021-02-19 23:09:13 -0600

    added burgers database with example text

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+
