ed1ebc88e56d5326ae8d83799c5321fedd1a23e7
Audrey King <audreymking1@gmail.com>
2021-02-20 21:36:41 -0600

    changed connection and node packages

--- config/connection.js
+++ config/connection.js
@@ -15,15 +15,15 @@ if (process.env.JAWSDB_URL) {
 };
 
 // Makes connection.
-// connection.connect((err) => {
-//   if (err) {
-//     console.error(`error connecting: ${err.stack}`);
-//     return;
-//   }
-//   console.log(`connected as id ${connection.threadId}`);
-// });
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
 
-connection.connect();
+//connection.connect();
 
 // Exports connection for the ORM to use.
 module.exports = connection;
